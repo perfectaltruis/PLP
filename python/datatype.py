4ec481bebc0e992b7be750ad09a86a1aef4909fc
Tito Joctan <perfect.altruistic@egmail.com>
2024-03-08 17:35:20 +0300

    Data type

--- python/datatype.py
+++ python/datatype.py
@@ -3,7 +3,6 @@ my_list = []
 # Append the following elements to my_list: 10, 20, 30, 40
 my_list.extend([10, 20, 30, 40])
 
-# Insert the value 15 at the second position in the list
 my_list.insert(1, 15)
 
 # Extend my_list with another list: [50, 60, 70]
