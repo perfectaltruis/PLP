5273ea37d527ae2d6b420a83993462878a7db9b0
Tito Joctan <perfect.altruistic@egmail.com>
2024-03-11 06:09:51 +0300

    discout calculator

--- python/discountcalculator.py
+++ python/discountcalculator.py
@@ -6,13 +6,15 @@ def calculate_discount(price, discount_percent):
     else:
         return price
 
+
 def main():
     original_price = float(input("Enter the original price of the item: "))
     discount_percent = float(input("Enter the discount percentage: "))
-    
+
     final_price = calculate_discount(original_price, discount_percent)
-    
+
     print(f"The final price after applying the discount is: {final_price}")
+
 
 if __name__ == "__main__":
     main()
