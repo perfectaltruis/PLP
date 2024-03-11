1072176f73d8c333ff4c190f1e040ef9bc034bb4
Tito Joctan <perfect.altruistic@egmail.com>
2024-03-11 06:10:35 +0300

    user inputs

--- python/user_input.py
+++ python/user_input.py
@@ -2,4 +2,4 @@ name = input("What is your name: ")
 age = int(input("What is your age: "))
 location = input("Where are you from: ")
 
-print(f"\nHello {name}, You are {age} years old and you come from '{location}'")
+print(f"\nHello {name}!, You are {age} years old and you come from '{location}'")
